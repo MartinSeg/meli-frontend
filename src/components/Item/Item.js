6e356ac0da7a7cca42b3776cb3100becc400ee00
Martin Seghezzo <martinseghezzo@gmail.com>
2021-03-10 10:15:25 -0300

    consumo del servicio de busqueda

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -3,12 +3,16 @@ import './Item.scss'
 import Shipping from '../../assets/ic_shipping.png'
 
 
-const Item = ({currency, amount, decimals, freeShipping }) => {
+const Item = ({currency, amount, decimals, freeShipping, picture }) => {
     return (
         <div className='item'>
             <div className='item-details'>
                 <div>
-                    <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdYSTpaHNx15fmP9aVxajM6sCYsGRij5NKIQ&usqp=CAU' alt='Product'/>
+                    <img 
+                        className='item-details-image'
+                        src={picture} 
+                        alt='Product'
+                    />
                 </div>
 
                 <div className='item-description'>
@@ -16,7 +20,7 @@ const Item = ({currency, amount, decimals, freeShipping }) => {
                         <span >
                             {currency} {amount},{decimals}
                         </span>
-                        {freeShipping && <img src={Shipping} alt='Shipping'/>}
+                        {freeShipping && <img className='item-details-shipping' src={Shipping} alt='Shipping'/>}
                     </div>
                     <span className='item-description-text'>
                         Apple iPod Touch 5g 16gb Negro, igual a Nuevo
--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -1,96 +1,113 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Item from '../Item'
-// import axios from 'axios'
+import axios from 'axios'
 import './Results.scss'
+import LoadingBox from '../LoadingBox'
 
-const mockItems = [ 
-    {
-        id: "1",
-        title: "hola",
-        price: {
-            currency: "$",
-            amount: 10,
-            decimals: 90
-        },
-        picture: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdYSTpaHNx15fmP9aVxajM6sCYsGRij5NKIQ&usqp=CAU",
-        condition: "paid",
-        free_shipping: false,   
-    },
-    {
-        id: "2",
-        title: "hola2",
-        price: {
-            currency: "$",
-            amount: 10,
-            decimals: 90
-        },
-        picture: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdYSTpaHNx15fmP9aVxajM6sCYsGRij5NKIQ&usqp=CAU",
-        condition: "paid",
-        free_shipping: true,   
-    },
-    {
-        id: "3",
-        title: "hola3",
-        price: {
-            currency: "$",
-            amount: 10,
-            decimals: 90
-        },
-        picture: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdYSTpaHNx15fmP9aVxajM6sCYsGRij5NKIQ&usqp=CAU",
-        condition: "paid",
-        free_shipping: true,   
-    },
-    {
-        id: "4",
-        title: "hola4",
-        price: {
-            currency: "$",
-            amount: 10,
-            decimals: 90
-        },
-        picture: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdYSTpaHNx15fmP9aVxajM6sCYsGRij5NKIQ&usqp=CAU",
-        condition: "paid",
-        free_shipping: true,   
-    }
-]
+// const mockItems = [ 
+//     {
+//         id: "1",
+//         title: "hola",
+//         price: {
+//             currency: "$",
+//             amount: 10,
+//             decimals: 90
+//         },
+//         picture: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdYSTpaHNx15fmP9aVxajM6sCYsGRij5NKIQ&usqp=CAU",
+//         condition: "paid",
+//         free_shipping: false,   
+//     },
+//     {
+//         id: "2",
+//         title: "hola2",
+//         price: {
+//             currency: "$",
+//             amount: 10,
+//             decimals: 90
+//         },
+//         picture: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdYSTpaHNx15fmP9aVxajM6sCYsGRij5NKIQ&usqp=CAU",
+//         condition: "paid",
+//         free_shipping: true,   
+//     },
+//     {
+//         id: "3",
+//         title: "hola3",
+//         price: {
+//             currency: "$",
+//             amount: 10,
+//             decimals: 90
+//         },
+//         picture: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdYSTpaHNx15fmP9aVxajM6sCYsGRij5NKIQ&usqp=CAU",
+//         condition: "paid",
+//         free_shipping: true,   
+//     },
+//     {
+//         id: "4",
+//         title: "hola4",
+//         price: {
+//             currency: "$",
+//             amount: 10,
+//             decimals: 90
+//         },
+//         picture: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdYSTpaHNx15fmP9aVxajM6sCYsGRij5NKIQ&usqp=CAU",
+//         condition: "paid",
+//         free_shipping: true,   
+//     }
+// ]
 
 
-const Results = () => {
+const Results = (props) => {
+
+    const [ loading, setLoading ] = useState(true)
+    const [ data, setData ] = useState({})
+    const searchProduct = props.location.search.split('=')[1];
 
     useEffect( () => {
-        // const callService = async () => {
-        //     try{
-        //         const data = await axios.get('http://localhost:5000')
-        //         console.log(data)
-        //     }catch(err){
-        //         console.log(err.response)
-        //     }
-        // }
+        const callService = async () => {
 
-        // callService();
+            try{
+                const {data} = await axios.get(`http://localhost:5000/api/items?q=${searchProduct}`)
+                console.log(data)
+                setData(data);
+                setLoading(false);
 
-    }, [])
+            }catch(err){
+                console.log(err.response.data.message)
+            }
+        }
+
+        callService();
+
+    }, [searchProduct])
 
     return (
         <>
-            <div className='breadCrum'>
-                ........
-            </div>
-            <div className='results'>     
-                <div className='results-main'>
-                    { mockItems.map( item => (
-                        <div key={item.id}>
-                            <Item 
-                                currency={item.price.currency}
-                                amount={item.price.amount}
-                                decimals={item.price.decimals}
-                                freeShipping={item.free_shipping}
-                            />
-                        </div>
-                    ))}
-                </div> 
-            </div>
-        </>
+        {loading
+            ? <LoadingBox /> 
+            :(
+                <>
+                    <div className='breadCrum'>
+                        ........
+                    </div>
+                    <div className='results'>  
+                        <ul>  
+                            { data.items.map( item => (
+                                <li key={item.id}>
+                                    <Item 
+                                        currency={item.price.currency}
+                                        amount={item.price.amount}
+                                        decimals={item.price.decimals}
+                                        freeShipping={item.free_shipping}
+                                        picture={item.picture}
+                                    />
+                                </li>
+                            ))}
+                        </ul> 
+                    </div>
+                </>
+            )
+        }
+        </>      
     )
 }
 
